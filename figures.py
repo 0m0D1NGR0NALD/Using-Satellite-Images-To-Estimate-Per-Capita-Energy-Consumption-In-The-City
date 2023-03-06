95552f857e2ae7c85462fcb8b4c91edd4983ff90
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-06 10:27:41 +0300

    Update figures.py

--- figures.py
+++ figures.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
-
+# Create dictionary with city population and available energy sources
 census_data = {
-
     'Freetown': {'population_size': '1,200,000', 
     'Energy Sources': 'BioMass, HydroPower, Solar'},
 
@@ -9,7 +8,8 @@ census_data = {
     'Energy Sources': 'HydroPower, Solar'}
 }
 
-
 def country_data(name):
+    # Load items in dictionary with census data
     data = census_data[name]
+    # Return conversion of dictionary to json file
     return jsonify({})
